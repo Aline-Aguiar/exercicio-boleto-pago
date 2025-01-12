4398ad62598a2fd392ba396e374c6601ad75be8c
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-11 23:44:14 -0300

    mudando de console.log para alert

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const boletoPago = false
 
 if(boletoPago){
-    console.log("O boleto está pago")
+    alert('O boleto está pago')
 }else{
-    console.log("O boleto não está pago")
+    alert('O boleto não está pago')
 }
 
 /*boletoPago ? console.log("O boleto está pago") : console.log("O boleto não está pago")*/
